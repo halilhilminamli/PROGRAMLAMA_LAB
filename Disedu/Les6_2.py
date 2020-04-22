0febdece092b62f9974b7780e99f4511274e09bc
halilhilminamli <44348411+halilhilminamli@users.noreply.github.com>
2020-04-22 07:11:02 +0300

    Create Les6_2.py

---
+++ Disedu/Les6_2.py
@@ -0,0 +1,8 @@
+from sympy import pprint
+
+from sympy import Symbol, exp, sqrt, pi, Integral 
+x = Symbol('x') 
+p = exp(-(x - 10)**2/2)/sqrt(2*pi)
+pprint(p)
+
+pprint(Integral(p, (x, 11, 12)).doit().evalf())
